d2529a3bbc8a0eb89b961472cf82e5e6f17a4aaa
Kousay Jebir <kousayjebir@gmail.com>
2024-08-17 21:20:28 +0100

    dynamically render schedule button

--- FRONTEND/src/components/NavigationTabs.jsx
+++ FRONTEND/src/components/NavigationTabs.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Tab, Tabs } from '@mui/material';
+import { Box, Tab, Tabs } from '@mui/material';
 import { useNavigate, useLocation } from 'react-router-dom';
-
+import Schedule from './Schedule';
 const NavigationTabs = ({ tabs, basePath }) => {
   const navigate = useNavigate();
   const location = useLocation();
+
+  function renderScheduleButton() {
+    if(location.pathname.startsWith('/recruitement')){
+      return <Schedule schedule={'interview'}/>
+    }
+    else if(location.pathname.startsWith('/meetings')) {
+      return <Schedule schedule={'meeting'}/>
+    }
+  }
+
+
 
   // Determine the index of the current tab based on the location
   const currentIndex = tabs.findIndex((path) => location.pathname.startsWith(`${basePath}${path}`));
@@ -14,7 +25,7 @@ const NavigationTabs = ({ tabs, basePath }) => {
   };
 
   return (
-    <Tabs
+    <Box p={1} pr={2} display='flex' alignItems='center' justifyContent={'space-between'} flexWrap={'wrap'} gap={1}><Tabs
       value={currentIndex !== -1 ? currentIndex : false}
       onChange={handleChange}
       aria-label="navigation tabs"
@@ -25,6 +36,8 @@ const NavigationTabs = ({ tabs, basePath }) => {
         <Tab key={path} label={path.slice(1)} sx={{ textTransform: 'capitalize' }} />
       ))}
     </Tabs>
+    {renderScheduleButton()}
+    </Box>
   );
 };
 
