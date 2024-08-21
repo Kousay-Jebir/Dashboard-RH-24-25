90b224be053f58d81e59b0534a0247920e026087
Kousay Jebir <kousayjebir@gmail.com>
2024-08-21 17:00:29 +0100

    drag and drop feature for kanban board

--- FRONTEND/src/Components/Recrutement/Schedule/KanbanBoard/InterviewStatus.jsx
+++ FRONTEND/src/Components/Recrutement/Schedule/KanbanBoard/InterviewStatus.jsx
@@ -1,11 +1,32 @@
 import { Box, Typography } from "@mui/material";
 import BorderBox from "../../../../components/BorderBox";
 import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
-export default function InterviewStatus({}){
+import RestoreOutlinedIcon from '@mui/icons-material/RestoreOutlined';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+export default function InterviewStatus({columnId,columnLength}){
+    let bgColor,textColor,title,icon;
+    if(columnId === 'confirmed'){
+         bgColor = 'success.main';
+         textColor = 'success.text';
+         title = 'Confirmed'
+         icon = <CheckCircleOutlineRoundedIcon sx={{color:textColor}} fontSize='small'/>
+    }
+    else if(columnId === 'delayed'){
+         bgColor = 'warning.main';
+         textColor = 'warning.text';
+         title = 'Delayed';
+         icon = <RestoreOutlinedIcon sx={{color:textColor}} fontSize='small'/>
+    }
+    else if(columnId === 'cancelled') {
+         bgColor = 'error.main';
+         textColor = 'error.text';
+         title = 'Cancelled';
+         icon = <CancelOutlinedIcon sx={{color:textColor}} fontSize='small'/>
+    }
     return(
-        <Box display={'flex'} gap={1} alignItems={'center'} bgcolor={'success.main'} borderRadius={1} paddingBlock={1} paddingInline={1.5} flexWrap={'wrap'} mb={1}>
-            <CheckCircleOutlineRoundedIcon sx={{color:"success.text"}} fontSize='small'/>
-            <Typography color={'#1DAF61'}>Confirmed(9)</Typography>
+        <Box display={'flex'} gap={1} alignItems={'center'} bgcolor={bgColor} borderRadius={1} paddingBlock={1} paddingInline={1.5} flexWrap={'wrap'} mb={1}>
+            {icon}
+            <Typography color={textColor}>{title +'('+columnLength+')'}</Typography>
         </Box>
     )
 }
--- FRONTEND/src/Components/Recrutement/Schedule/KanbanBoard/Layout.jsx
+++ FRONTEND/src/Components/Recrutement/Schedule/KanbanBoard/Layout.jsx
@@ -2,30 +2,101 @@ import { Grid } from "@mui/material";
 import InterviewStatus from "./InterviewStatus";
 import RecruitementInterviewCard from "./RecruitementInterviewCard";
 import InterviewsData from './KanbanBoardData.json'
+import { useState } from "react";
+import { DndContext } from "@dnd-kit/core";
+import { useDroppable } from "@dnd-kit/core";
+import { columns } from "../../../../lib/react-beautiful-dnd/kanban-board/data";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function Layout() {
+    const [boardColumns,setBoardColumns] = useState(columns)
+    const onDragEnd = (result,boardColumns,setBoardColumns)=>{
+        if(!result.destination){
+            return
+        }
+        const {source,destination} = result;
+        if(source.droppableId !== destination.droppableId){
+            const sourceColumn = boardColumns[source.droppableId];
+            const destColumn = boardColumns[destination.droppableId];
+            const sourceItems = [...sourceColumn.items];
+            const destItems = [...destColumn.items];
+            const [removed]= sourceItems.splice(source.index,1);
+            destItems.splice(destination.index,0,removed);
+            setBoardColumns({
+                ...boardColumns,
+                [source.droppableId] : {
+                    ...sourceColumn,
+                    items:sourceItems
+                },
+                [destination.droppableId] : {
+                    ...destColumn,
+                    items:destItems
+                }
+            })
+        }
+        else{
+            const column = boardColumns[source.droppableId];
+            const copiedItems = [...column.items];
+            const [removed]=copiedItems.splice(source.index,1);
+            copiedItems.splice(destination.index,0,removed);
+            setBoardColumns({
+                ...boardColumns,
+                [source.droppableId]:{
+                    ...column,
+                    items:copiedItems
+                }
+            })
+        }       
+        console.log(boardColumns)
+    }
     return(
-        <Grid container xs={12} sx={{overflow:'scroll'}} spacing={2}>
-            <Grid item xs={4}>
-                <InterviewStatus/>
-                {InterviewsData.map((interview, index) => (
-        <RecruitementInterviewCard key={index} interview={interview} />
-      ))}
+        <DragDropContext onDragEnd={result => onDragEnd(result,boardColumns,setBoardColumns)}>
+            <Grid container xs={12} sx={{overflow:'scroll'}} spacing={2}>
+            {
+                Object.entries(boardColumns).map(([id,column])=>{
+                    return(
+                        <Droppable droppableId={id} key={id}>
+                            {(provided,snapshot)=>{
+                                return (
+                                    <Grid item xs={4}
+                                    {...provided.droppableProps}
+                                    ref={provided.innerRef}
+                                    style={{
+                                        
+                                    }}
+                                    >   
+                                    <InterviewStatus columnId={id} columnLength={column.items.length}/>
+                                        {column.items.map((item,index)=>{
+                                            return(
+                                                <Draggable key={item.id} draggableId={item.id} index={index}>
+                                                    {
+                                                        (provided,snapshot)=>{
+                                                            return(
+                                                                <div ref={provided.innerRef} 
+                                                                    {...provided.draggableProps}
+                                                                    {...provided.dragHandleProps}
+                                                                    style={{
+                                                                        userSelect:'none',
+                                                                        ...provided.draggableProps.style
+                                                                    }}>
+                                                                        <RecruitementInterviewCard  interview={item}/>
+                                                                </div>
+                                                            )
+                                                        }
+                                                    }
+                                                </Draggable>
+                                            )
+                                        })}
+                                        {provided.placeholder}
+                                    </Grid>
+                                )
+                            }}
+                        </Droppable>
+                    )
+                })
+            }
             </Grid>
-
-            <Grid item xs={4}>
-                <InterviewStatus/>
-                {InterviewsData.map((interview, index) => (
-        <RecruitementInterviewCard key={index} interview={interview} />
-      ))}
-            </Grid>
-
-            <Grid item xs={4}>
-                <InterviewStatus/>
-                {InterviewsData.map((interview, index) => (
-        <RecruitementInterviewCard key={index} interview={interview} />
-      ))}
-            </Grid>
-        </Grid> 
+        </DragDropContext> 
+        
     )
 }
