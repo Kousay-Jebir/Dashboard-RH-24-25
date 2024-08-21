76fe12370ae4a96a172fd3aad7ce91e50523806d
Kousay Jebir <kousayjebir@gmail.com>
2024-08-21 17:23:43 +0100

    disable elevation on schedule button

--- FRONTEND/src/components/Schedule.jsx
+++ FRONTEND/src/components/Schedule.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 
-export default function Schedule({schedule}) {
+export default function Schedule({schedule,isOutlined}) {
     return (
         <Button
             variant='contained'
             startIcon={<AddIcon />} // Adds the plus icon at the start
+            disableElevation
             sx={{
                 textTransform: 'none', // Ensures text is not in all caps
-                
+                borderRadius:1.5
             }}
         >
             {"Schedule "+ schedule}
