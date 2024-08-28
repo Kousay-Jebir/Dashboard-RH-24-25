44f663970307e199585a6e50817e3f64c266233b
youssefab-11 <youssef.abbes111@gmail.com>
2024-08-28 16:14:12 +0100

    date filter is functional

--- FRONTEND/src/Components/Recrutement/Interviews/DateFilter.jsx
+++ FRONTEND/src/Components/Recrutement/Interviews/DateFilter.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
 
-const DateFilter = () => {
+const DateFilter = ({ onDateChange }) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker />
+      <DatePicker
+        onChange={(date) => {
+          const formattedDate = date ? dayjs(date).format("DD-MM-YYYY") : null;
+          onDateChange(formattedDate);
+        }}
+        format="DD-MM-YYYY"
+      />
     </LocalizationProvider>
   );
 };
--- FRONTEND/src/Pages/Recrutement/Interviews.jsx
+++ FRONTEND/src/Pages/Recrutement/Interviews.jsx
@@ -1,19 +1,37 @@
-import { Box, useTheme ,Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, useTheme } from "@mui/material";
 import InterviewsDataGrid from "../../Components/Recrutement/Interviews/InterviewsDataGrid";
 import DateFilter from "../../Components/Recrutement/Interviews/DateFilter";
+import data from "../../Components/Recrutement/Interviews/InterviewsData.json";
+import dayjs from "dayjs";
 
-export default function Interviews(){
-    const theme = useTheme();
-    return(
-        <Box sx={{
-            border: 2,
-            borderRadius: 2,
-            borderColor: theme.palette.neutral.light,
-            margin: 2,
-            padding: 2,
-          }}>
-            <DateFilter/>
-            <InterviewsDataGrid/>
-        </Box>
-    )
+export default function Interviews() {
+  const theme = useTheme();
+  const [filteredData, setFilteredData] = useState(data);
+
+  const handleDateChange = (selectedDate) => {
+    if (selectedDate) {
+      const filtered = data.filter((row) =>
+        dayjs(row.date, "DD-MM-YYYY").format("DD-MM-YYYY") === selectedDate
+      );
+      setFilteredData(filtered);
+    } else {
+      setFilteredData(data); // Reset to all data if no date is selected
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        border: 2,
+        borderRadius: 2,
+        borderColor: theme.palette.neutral.light,
+        margin: 2,
+        padding: 2,
+      }}
+    >
+      <DateFilter onDateChange={handleDateChange} />
+      <InterviewsDataGrid data={filteredData} />
+    </Box>
+  );
 }
