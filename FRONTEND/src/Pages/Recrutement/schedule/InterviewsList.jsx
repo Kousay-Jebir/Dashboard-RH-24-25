964027e0b84e71923e9804b8f2afa1c3251a2fce
youssefab-11 <youssef.abbes111@gmail.com>
2024-08-29 15:45:00 +0100

    braches have been merged into dev

--- FRONTEND/src/Pages/Recrutement/schedule/InterviewsList.jsx
+++ FRONTEND/src/Pages/Recrutement/schedule/InterviewsList.jsx
@@ -1,13 +1,19 @@
-import { Box } from "@mui/material"
-import ScheduleHeader from "./ScheduleHeader"
-import StatusBar from "../../../Components/Recrutement/Schedule/list/StatusBar"
-export default function InterviewsList(){
-    return(
-        <Box >
-            <Box mb={2}><ScheduleHeader/></Box>
-            <Box mb={2}>
-                <StatusBar></StatusBar>
-            </Box>
-        </Box>
-    )
+import { Box } from "@mui/material";
+import ScheduleHeader from "./ScheduleHeader";
+import StatusBar from "../../../Components/Recrutement/Schedule/list/StatusBar";
+import ScheduleDataGrid from "../../../Components/Recrutement/Schedule/List/ScheduleDataGrid";
+export default function InterviewsList() {
+  return (
+    <Box>
+      <Box mb={2}>
+        <ScheduleHeader />
+      </Box>
+      <Box mb={2}>
+        <StatusBar></StatusBar>
+      </Box>
+      <Box mb={2}>
+        <ScheduleDataGrid />
+      </Box>
+    </Box>
+  );
 }
