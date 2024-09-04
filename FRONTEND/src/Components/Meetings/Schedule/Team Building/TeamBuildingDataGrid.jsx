5e26d09b62649b9f66a4db82a65a1c1face07ee4
Kousay Jebir <kousayjebir@gmail.com>
2024-09-04 19:48:52 +0100

    fix wrong import

--- FRONTEND/src/Components/Meetings/Schedule/Team Building/TeamBuildingDataGrid.jsx
+++ FRONTEND/src/Components/Meetings/Schedule/Team Building/TeamBuildingDataGrid.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import DataTable from "../../../DataTable";
-import BorderBox from "../../../BorderBox";
+import BorderBox from "../../../../components/BorderBox";
 
 const columns = [
   { id: "Title", label: "Meeting title" },
