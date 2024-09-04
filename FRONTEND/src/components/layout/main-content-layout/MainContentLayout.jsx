29bc15e0f6373fac06f34c95bd2299a17e63fc58
Kousay Jebir <kousayjebir@gmail.com>
2024-09-04 19:53:30 +0100

    fix empty select menu showing up

--- FRONTEND/src/components/layout/main-content-layout/MainContentLayout.jsx
+++ FRONTEND/src/components/layout/main-content-layout/MainContentLayout.jsx
@@ -26,11 +26,12 @@ export default function MainContentLayout() {
   };
 
   const menuData = getMenuData();
-
+  console.log("menu data is")
+  console.log(menuData)
   return (
     <Grid container p={3} pt={4} spacing={2}>
       <Grid item xs={12} sm={4} md={2.5}>
-        <SelectMenu menuData={menuData} />
+        { menuData.length>0 ? <SelectMenu menuData={menuData} /> : null}
       </Grid>
       <Grid item xs={12} sm={8} md={9.5} sx={{overflow:'auto'}}>
         <Outlet />
