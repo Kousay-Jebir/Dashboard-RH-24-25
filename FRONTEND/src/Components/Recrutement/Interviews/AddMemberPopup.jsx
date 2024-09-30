7ba4678214d0a82023cb719b64bbcbed6879e81e
youssefab-11 <youssef.abbes111@gmail.com>
2024-09-30 21:53:29 +0100

    fixed styles

--- FRONTEND/src/Components/Recrutement/Interviews/AddMemberPopup.jsx
+++ FRONTEND/src/Components/Recrutement/Interviews/AddMemberPopup.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton } from '@mui/material';
-import WarningIcon from '@mui/icons-material/Warning';
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  useTheme,
+} from "@mui/material";
+import { GoAlertFill } from "react-icons/go";
 
 const AddMemberPopup = () => {
+  const theme = useTheme();
+
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
@@ -29,23 +40,61 @@ const AddMemberPopup = () => {
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        sx={{padding:2}}
       >
-          <WarningIcon sx={{m:2 , marginInline:"auto"}} />
-        <DialogTitle id="alert-dialog-title" style={{ display: 'flex', alignItems: 'center' }}>
+        <GoAlertFill
+          style={{ marginTop: 10, marginInline: "auto", fontSize: 50 }}
+        />
+        <DialogTitle
+          id="alert-dialog-title"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            fontSize: "18px",
+            justifyContent: "center",
+          }}
+        >
           Are you sure you want to add this member to your team?
         </DialogTitle>
 
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Confirming this action will grant the member access to team resources and responsibilities. Please ensure you have reviewed their qualifications and suitability for the role.
+          <DialogContentText
+            style={{
+              fontSize: "12px",
+              textAlign: 'center'
+            }}
+            id="alert-dialog-description"
+          >
+            Confirming this action will grant the member access to team
+            resources and responsibilities. Please ensure you have reviewed
+            their qualifications and suitability for the role.
           </DialogContentText>
         </DialogContent>
 
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
+        <DialogActions sx={{padding:2}}>
+          <Button
+            onClick={handleClose}
+            variant="text"
+            sx={{
+              border: 2,
+              borderRadius: 2,
+              borderColor: theme.palette.neutral.light,
+              width: "25%",
+              color: theme.palette.neutral.normal,
+            }}
+          >
             Back
           </Button>
-          <Button onClick={handleConfirm} color="warning" variant="contained">
+          <Button
+            onClick={handleConfirm}
+            variant="contained"  
+            sx={{
+              border: 2,
+              borderRadius: 2,
+              borderColor: theme.palette.primary.main,
+              width: "75%",
+            }}
+          >
             Yes I am
           </Button>
         </DialogActions>
