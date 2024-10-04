a17e4225063d6fceeda759d25e7bcdc819884f18
EmnaWalha99 <ewalha12@gmail.com>
2024-10-04 09:17:28 +0100

    update the kanban board form
    error handling

--- FRONTEND/src/Components/kanbanForm.jsx
+++ FRONTEND/src/Components/kanbanForm.jsx
@@ -1,19 +1,23 @@
+import ForumRoundedIcon from "@mui/icons-material/ForumRounded";
 import { Box, Button, FormControl, FormControlLabel, Radio, RadioGroup, TextField, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import React, { useState } from 'react';
 import { departments } from './Recrutement/jei-departments';
 
-const KanbanForm = ({getFormData}) => {
+const KanbanForm = ({ getFormData }) => {
     const theme = useTheme();
 
-    // State to manage form data
     const [formData, setFormData] = useState({
         interviewWith: '',
         interviewedBy: '',
-        department: departments.PROJET.title
+        department: departments.PROJET.title,
+        date: '',
+        time: ''
     });
 
-    // Handler for input changes
+    const [errors, setErrors] = useState({});
+    const [showDateTime, setShowDateTime] = useState(false);
+
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormData((prevState) => ({
@@ -22,21 +26,66 @@ const KanbanForm = ({getFormData}) => {
         }));
     };
 
-    // Handler for form submission
     const handleSubmit = () => {
-        getFormData(formData);
+        if (validateForm()) {
+            getFormData(formData);
+        }
     };
 
-    const renderTextField = (label, name, width, placeholder) => (
-        <Box sx={{ width: '216px', height: '20px', display: 'flex', flexDirection: 'row', gap: '6px' }}>
+    const toggleDateTimeFields = () => {
+        setShowDateTime(prev => !prev);
+    };
+
+    const validateForm = () => {
+        const newErrors = {};
+
+        // Utility function to check for invalid characters
+        const hasInvalidCharacters = (value) => /[^a-zA-Z\s\-\'èéàâôîû]/.test(value);
+
+        //interview with validation
+        if (!formData.interviewWith.trim()) {
+            newErrors.interviewWith = "The interviewee's name is required.";
+        } else if (hasInvalidCharacters(formData.interviewWith)) {
+            newErrors.interviewWith = "The interviewee's name contains invalid characters.";
+        } else if (formData.interviewWith.length < 3) {
+            newErrors.interviewWith = "The interviewee's name is too short";
+        } else if (formData.interviewWith.length > 20) {
+            newErrors.interviewWith = "The interviewee's name is too long.";
+        }
+
+        //interviewed by validation 
+        if (!formData.interviewedBy.trim()) {
+            newErrors.interviewedBy = "The interviewer's name is required.";
+        } else if (hasInvalidCharacters(formData.interviewedBy)) {
+            newErrors.interviewedBy = "The interviewer's name contains invalid characters.";
+        } else if (formData.interviewedBy.length < 3) {
+            newErrors.interviewedBy = "The interviewer's name is too short.";
+        } else if (formData.interviewedBy.length > 20) {
+            newErrors.interviewedBy = "The interviewer's name is too long.";
+        }
+
+        if (!formData.date)
+        {
+            newErrors.date="Date is required";
+        }
+        if(!formData.time)
+        {
+            newErrors.time="Time is required";
+        }
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
+    const renderTextField = (label, name, placeholder, type = 'text') => (
+        <Box sx={{ height: '28px', display: 'flex', flexDirection: 'row', gap: '6px' }}>
             <Typography
                 variant="body2"
                 sx={{
                     fontFamily: theme.typography.fontFamily,
-                    fontSize: '10px',
+                    fontSize: '11px',
                     fontWeight: theme.typography.regular,
-                    lineHeight: '12.1px',
-                    textAlign: 'left',
+                    color: theme.palette.neutral.normal
                 }}
             >
                 {label}
@@ -46,52 +95,80 @@ const KanbanForm = ({getFormData}) => {
                 value={formData[name]}
                 onChange={handleChange}
                 placeholder={placeholder}
+                type={type}
+                error={!!errors[name]}
+                helperText={errors[name]}
                 sx={{
-                    height: '20px',
-                    width: width,
+                    width: '100%',
                     '& .MuiInputBase-root': { height: '100%' },
                     '& .MuiInputBase-input': { fontFamily: theme.typography.fontFamily, fontSize: '10px' },
+                    '& .MuiFormHelperText-root': {
+                        fontSize: '8px',  
+                        color: 'red',
+                        margin: 0,  
+                    },
+                    marginBottom:1,
+                    '& input:-webkit-autofill': {
+                        WebkitBoxShadow: '0 0 0 1000px white inset',
+                        WebkitTextFillColor: 'black',
+                        fontFamily: theme.typography.fontFamily,
+                        transition: 'background-color 5000s ease-in-out 0s',
+    },
+
                 }}
+                
             />
+            
         </Box>
     );
 
     return (
-        <Box sx={{
-            width: '236px',
-            height: '260px',
-            gap: '7px',
-            border: '1px solid lightGrey',
-            paddingLeft: '10px',
-            paddingTop: '10px'
-        }}>
-            <Box
-                sx={{
-                    width: '216px',
-                    height: '84px',
-                    gap: '8px',
-                    display: 'flex',
-                    flexDirection: 'column'
-                }}
-            >
+        <Box
+            border={1}
+            borderRadius={2}
+            borderColor={theme.palette.neutral.light}
+            p={1}
+            mb={1}
+        >
+            <Box>
+                <Box display="flex" flexDirection="row" alignItems="left">
+                    <ForumRoundedIcon
+                        sx={{
+                            color: theme.palette.neutral.normal,
+                            width: 12,
+                            height: 12,
+                        }}
+                    />
+                    <Typography fontSize={11} color={theme.palette.neutral.normal} ml={0.5} mb={1.5}>
+                        Recruitment interview
+                    </Typography>
+                </Box>
+                <Box mb={1}>
+                    {renderTextField('Interview with', 'interviewWith', "Enter name")}
+                </Box>
+
                 <Typography
-                    variant="h6"
                     sx={{
                         fontFamily: theme.typography.fontFamily,
-                        fontSize: '10px',
-                        fontWeight: theme.typography.regular,
-                        lineHeight: '12.1px',
-                        textAlign: 'left',
+                        fontSize: 11,
+                        color: theme.palette.neutral.normal,
+                        marginBottom: 1.5,
+                        cursor: 'pointer'
                     }}
+                    onClick={toggleDateTimeFields}
                 >
-                    Recruitment Interview
+                    + Add date & time
                 </Typography>
 
-                <Box sx={{ display: 'flex', gap: '6px' }}>
-                    {renderTextField('Interview with', 'interviewWith', 142, "Enter name")}
-                </Box>
-                <Box sx={{ display: 'flex', gap: '6px' }}>
-                    {renderTextField('Will be interviewed by', 'interviewedBy', 108, "Enter name")}
+                {showDateTime && (
+                    <Box sx={{ display: 'flex', flexDirection: 'column', gap:1, mb: 1 }}>
+                        {renderTextField('Date', 'date', '', 'date')}
+                        {renderTextField('Time', 'time', '', 'time')}
+                    </Box>
+                )}
+
+                <Box width={243} mb={1}>
+                    {renderTextField('Will be interviewed by', 'interviewedBy', "Enter name")}
                 </Box>
             </Box>
 
@@ -100,7 +177,8 @@ const KanbanForm = ({getFormData}) => {
                     width: '216px',
                     height: '102px',
                     gap: '8px',
-                    paddingTop: '10px'
+                    paddingTop: '10px',
+                    marginTop:2
                 }}
             >
                 <FormControl component="fieldset">
@@ -108,10 +186,11 @@ const KanbanForm = ({getFormData}) => {
                         variant="subtitle1"
                         sx={{
                             fontFamily: theme.typography.fontFamily,
-                            fontSize: '10px',
+                            fontSize: '11px',
                             fontWeight: theme.typography.regular,
                             lineHeight: '12.1px',
                             textAlign: 'left',
+                            marginBottom: 1
                         }}
                     >
                         Choose department
@@ -121,19 +200,31 @@ const KanbanForm = ({getFormData}) => {
                         name="department"
                         value={formData.department}
                         onChange={handleChange}
-                        sx={{ display: 'flex', flexDirection: 'row', gap: '10px', marginLeft: '10px', marginTop: '5px' }}
+                        sx={{ display: 'flex', flexDirection: 'row', gap: '4px', marginLeft: '10px', marginTop: '5px' }}
                     >
                         {Object.values(departments).map(
                             (dept) => (
                                 <FormControlLabel
                                     key={dept.id}
                                     value={dept.title}
+                                    sx={{
+                                        border: 1,
+                                        borderColor: theme.palette.neutral.light,
+                                        borderRadius: 2,
+                                        padding: '2px 6px'
+                                    }}
                                     control={
                                         <Radio
                                             sx={{
-                                                width: '12px',
-                                                height: '12px',
-                                                marginRight: '4px'
+                                                height: 20,
+                                                width: 20,
+                                                borderColor: theme.palette.neutral,
+                                                borderWidth: 1,
+                                                '& .MuiSvgIcon-root': { fontSize: '16px' },
+                                                color: theme.palette.neutral,
+                                                '&.Mui-checked': {
+                                                    color: "#6A7177",
+                                                },
                                             }}
                                         />
                                     }
@@ -141,7 +232,7 @@ const KanbanForm = ({getFormData}) => {
                                         <Typography
                                             sx={{
                                                 fontFamily: theme.typography.fontFamily,
-                                                fontSize: '10px',
+                                                fontSize: '9px',
                                                 fontWeight: theme.typography.regular,
                                                 lineHeight: '12.1px',
                                                 textAlign: 'left',
@@ -162,17 +253,32 @@ const KanbanForm = ({getFormData}) => {
                 <Button
                     onClick={handleSubmit}
                     sx={{
-                        width: '216px',
+                        width: '235px',
                         height: '24px',
-                        padding: '6px 6px 6px 6px',
+                        padding: '6px 6px',
                         gap: '8px',
-                        color: 'white',
+                        color: theme.palette.white.main,
                         borderRadius: '4px',
                         border: `1px solid ${theme.palette.neutral}`,
                         backgroundColor: '#404951',
                         textAlign: 'center',
                         fontSize: '10px',
                         marginTop: '30px',
+                        marginLeft: '5px',
+                        marginRight: '5px',
+                        fontFamily: theme.typography.fontFamily,
+                        '&:hover': {
+                            backgroundColor: '#404951',
+                            color: theme.palette.white.main,
+                        },
+                        '&:active': {
+                            backgroundColor: '#404951',
+                            color: theme.palette.white.main,
+                        },
+                        '&:focus': {
+                            backgroundColor: '#404951',
+                            color: theme.palette.white.main,
+                        },
                     }}
                 >
                     Confirm
