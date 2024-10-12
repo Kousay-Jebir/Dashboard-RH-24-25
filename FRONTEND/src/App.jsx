391a51a949b49e51a656e7867cbba6f428adfe97
Kousay Jebir <kousayjebir@gmail.com>
2024-10-12 12:46:54 +0100

    fix import bug due to merge

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -9,7 +9,6 @@ import AppLayout from './components/layout/global-layout/AppLayout';
 import MainContentLayout from './components/layout/main-content-layout/MainContentLayout';
 
 import Dashoboard from './Pages/Dashboard/Dashoboard';
-import Login from './Pages/Login/Login';
 import DepartmentMeetings from './Pages/Meetings/DepartmentMeetings';
 import Event from './Pages/Meetings/Event';
 import GeneralAssembly from './Pages/Meetings/GeneralAssembly';
@@ -20,10 +19,6 @@ import InterviewsList from './Pages/Recrutement/schedule/InterviewsList';
 import KanbanBoard from './Pages/Recrutement/schedule/KanbanBoard';
 import TeamMembers from './Pages/Team members/TeamMembers';
 import AllQuestions from './Components/Recrutement/Interviews/Questions/AllQuestions';
-import GlobalForm from "./Components/Recrutement/Interviews/Questions/Global";
-import ScoresForm from "./Components/Recrutement/Interviews/Questions/ScoresForm";
-import {jwtDecode} from 'jwt-decode';
-import LoginForm from "./components/LoginForm";
 import LoginV2 from "./Pages/Login/LoginV2";
 import { NotificationProvider } from "./context/SnackBarContext";
 
