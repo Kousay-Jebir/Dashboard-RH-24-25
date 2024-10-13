d6e8bff04ad4524c8ed4bf8d1058fb26eead39df
Kousay Jebir <kousayjebir@gmail.com>
2024-10-13 19:12:31 +0100

    fix import bug

--- FRONTEND/src/Pages/Dashboard/Dashoboard.jsx
+++ FRONTEND/src/Pages/Dashboard/Dashoboard.jsx
@@ -1,6 +1,6 @@
 import { Box, Drawer, Grid, Typography, useTheme } from "@mui/material";
 import React, { useState } from "react";
-import ScheduleInterview from "../../Components/Recrutement/ScheduleInterview";
+
 
 import AllTicketsKPI from "../../Components/AllTicketsKPI";
 import MembersByCategory from "../../Components/Dashboard/MembersByCategory";
