adc468375c889ca5b3c9a67f36def3c9b31af722
youssefab-11 <youssef.abbes111@gmail.com>
2024-10-14 13:50:58 +0100

    integrate show Qr code and fixing styles

--- FRONTEND/src/Components/Meetings/MeetingDetails/QrCodeDialog.jsx
+++ FRONTEND/src/Components/Meetings/MeetingDetails/QrCodeDialog.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from '@mui/material';
-import { QRCode } from 'qrcode.react';
+import { useTheme } from "@emotion/react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Typography,
+} from "@mui/material";
+import { QRCodeSVG } from "qrcode.react";
 
 const QRCodeDialog = ({ open, handleClose }) => {
-  const qrValue = 'https://example.com'; // Valeur du QR code
+  const qrValue = "https://example.com"; // Valeur du QR code
+
+  const theme = useTheme();
 
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Scan QR code to confirm your presence</DialogTitle>
+    <Dialog open={open} onClose={handleClose} maxWidth="xs" fullWidth>
+      <DialogTitle sx={{ fontSize: "14px" , color: theme.palette.text.main }} align="center">
+        Scan QR code to confirm your presence
+      </DialogTitle>
       <DialogContent>
-        <Typography variant="body2" align="center" style={{ marginTop: '16px' }}>
-          Please scan this QR code to confirm your presence.
-        </Typography>
-        <div style={{ textAlign: 'center' }}>
-          <QRCode value={qrValue} size={256} />
+        <div style={{ textAlign: "center" }}>
+          <QRCodeSVG value={qrValue} size={250} />
         </div>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="primary">
+        <Button
+          onClick={handleClose}
+          variant="outlined"
+          sx={{
+            width: "100%",
+            mb: 2,
+            color: theme.palette.neutral.normal,
+            borderColor: theme.palette.neutral.light,
+            "&:hover": {
+              backgroundColor: theme.palette.neutral.light,
+              borderColor: theme.palette.neutral.light,
+            },
+          }}
+        >
           Close
         </Button>
       </DialogActions>
